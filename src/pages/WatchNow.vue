3af3a27e4beb9650117da5e70c9462d5c69be525
Eduardo <lsceduardogp@gmail.com>
2020-06-24 20:11:28 -0500

    update watch

--- src/pages/WatchNow.vue
+++ src/pages/WatchNow.vue
@@ -1,35 +1,90 @@
 <template>
-  <div>
-    <video-player
-      class="video-player-box"
-      ref="videoPlayer"
-      :options="playerOptions"
-      :playsinline="true"
-      customEventName="videouno"
-      @play="onPlayerPlay($event)"
-      @pause="onPlayerPause($event)"
-      @ended="onPlayerEnded($event)"
-      @waiting="onPlayerWaiting($event)"
-      @statechanged="playerStateChanged($event)"
-      @ready="playerReadied">
-    </video-player>
-    <video-player
-      class="vjs-custom-skin"
-      ref="videoPlayer"
-      :options="playerOptions"
-      :playsinline="true"
-      customEventName="videodos"
-      @play="onPlayerPlay($event)"
-      @pause="onPlayerPause($event)"
-      @ended="onPlayerEnded($event)"
-      @waiting="onPlayerWaiting($event)"
-      @statechanged="playerStateChanged($event)"
-      @ready="playerReadied">
-    </video-player>
+  <div class="q-pa-md q-gutter-sm">
+    <div class="row justify-center">
+      <q-btn-toggle
+        glossy
+        v-model="slide"
+        :options="[
+          { label: 'CÁMARA 1', value: 'camara-uno' },
+          { label: 'CÁMARA 2', value: 'camara-dos' },
+          { label: 'CÁMARA 3', value: 'camara-tres' },
+          { label: 'CÁMARA 4', value: 'camara-cuatro' }
+        ]"
+      />
+    </div>
+    <q-carousel
+      animated
+      v-model="slide"
+      infinite
+    >
+      <q-carousel-slide name="camara-uno">
+        <video-player
+          class="video-player-box"
+          ref="videoPlayer"
+          :options="playerOptions"
+          :playsinline="true"
+          customEventName="camarauno"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+          @waiting="onPlayerWaiting($event)"
+          @statechanged="playerStateChanged($event)"
+          @ready="playerReadied">
+        </video-player>
+      </q-carousel-slide>
+      <q-carousel-slide name="camara-dos">
+        <video-player
+          class="video-player-box"
+          ref="videoPlayer"
+          :options="playerOptionsDos"
+          :playsinline="true"
+          customEventName="camarados"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+          @waiting="onPlayerWaiting($event)"
+          @statechanged="playerStateChanged($event)"
+          @ready="playerReadied">
+        </video-player>
+      </q-carousel-slide>
+      <q-carousel-slide name="camara-tres">
+        <video-player
+          class="video-player-box"
+          ref="videoPlayer"
+          :options="playerOptionsTres"
+          :playsinline="true"
+          customEventName="camaratres"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+          @waiting="onPlayerWaiting($event)"
+          @statechanged="playerStateChanged($event)"
+          @ready="playerReadied">
+        </video-player>
+      </q-carousel-slide>
+      <q-carousel-slide name="camara-cuatro">
+        <video-player
+          class="video-player-box"
+          ref="videoPlayer"
+          :options="playerOptionsCuatro"
+          :playsinline="true"
+          customEventName="camaracuatro"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+          @waiting="onPlayerWaiting($event)"
+          @statechanged="playerStateChanged($event)"
+          @ready="playerReadied">
+        </video-player>
+      </q-carousel-slide>
+    </q-carousel>
   </div>
 </template>
-<style lang="stylus" scoped>
-</style>
+<style lang="scss" scoped>
+.q-carousel{
+  height: auto;
+}
+</style>>
 <script>
 import 'video.js/dist/video-js.css'
 import { videoPlayer } from 'vue-video-player'
@@ -40,21 +95,17 @@ export default {
   },
   data () {
     return {
+      slide: 'camara-uno',
       playerOptions: {
         playbackRates: [0.5, 1.0, 1.5, 2.0],
         autoplay: false,
         muted: false,
-        loop: false,
+        loop: true,
         preload: 'auto',
-        language: 'zh-CN',
+        language: 'en',
         aspectRatio: '16:9',
         fluid: true,
         sources: [
-          {
-            type: 'application/x-mpegURL',
-            src:
-              'https://videodelivery.net/cf4aca19b74c35d176679c6f7b1b13e7/manifest/video.m3u8'
-          },
           {
             type: 'application/x-mpegURL',
             src:
@@ -62,8 +113,9 @@ export default {
           }
         ],
         hls: true,
-        poster: 'http://pic33.nipic.com/20131007/13639685_123501617185_2.jpg',
+        // poster: 'statics/images/theater-wallpaper.jpg',
         width: document.documentElement.clientWidth, // Player width
+        // height: '360',
         notSupportedMessage: 'This video cannot be played for the time being. Please try again later.',
         controlBar: {
           timeDivider: true,
@@ -79,29 +131,75 @@ export default {
           },
           playbackRateMenuButton: true */
         }
+      },
+      playerOptionsDos: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0],
+        autoplay: false,
+        muted: false,
+        loop: false,
+        language: 'en',
+        // aspectRatio: '16:9',
+        fluid: true,
+        sources: [
+          {
+            type: 'application/x-mpegURL',
+            src:
+              'https://videodelivery.net/ec6a3598060ce2fca1ff5f450a003c72/manifest/video.m3u8'
+          }
+        ],
+        hls: false,
+        poster: 'statics/images/theater-wallpaper.jpg',
+        width: document.documentElement.clientWidth, // Player width
+        height: '360',
+        notSupportedMessage: 'This video cannot be played for the time being. Please try again later.'
+      },
+      playerOptionsTres: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0],
+        autoplay: false,
+        muted: false,
+        loop: false,
+        language: 'en',
+        // aspectRatio: '16:9',
+        fluid: true,
+        sources: [
+          {
+            type: 'application/x-mpegURL',
+            src:
+              'https://videodelivery.net/d877d4217c7a0683c35ac12fc5aa8d94/manifest/video.m3u8'
+          }
+        ],
+        hls: false,
+        poster: 'statics/images/theater-wallpaper.jpg',
+        width: document.documentElement.clientWidth, // Player width
+        height: '360',
+        notSupportedMessage: 'This video cannot be played for the time being. Please try again later.'
+      },
+      playerOptionsCuatro: {
+        playbackRates: [0.5, 1.0, 1.5, 2.0],
+        autoplay: false,
+        muted: false,
+        loop: false,
+        language: 'en',
+        // aspectRatio: '16:9',
+        fluid: true,
+        sources: [
+          {
+            type: 'application/x-mpegURL',
+            src:
+              'https://videodelivery.net/cf4aca19b74c35d176679c6f7b1b13e7/manifest/video.m3u8'
+          }
+        ],
+        hls: false,
+        poster: 'statics/images/theater-wallpaper.jpg',
+        width: document.documentElement.clientWidth, // Player width
+        height: '360',
+        notSupportedMessage: 'This video cannot be played for the time being. Please try again later.'
       }
     }
   },
   mounted () {
     console.log('this is current player instance object', this.player)
-    this.$refs.videoPlayer.player.controlBar.progressControl.disable()
-    setTimeout(() => {
-      console.log('dynamic change options', this.playerOptions)
-      // this.player.src(url);
-      // change src
-      // this.playerOptions.sources[0].src = 'https://videodelivery.net/d030fb826be3b9395b9c46bfcd3eabf8/manifest/video.m3u8'
-      // change item
-      // this.$set(this.playerOptions.sources, 0, {
-      //   type: "application/x-mpegURL",
-      //   src: 'https://videodelivery.net/d030fb826be3b9395b9c46bfcd3eabf8/manifest/video.m3u8',
-      // })
-      // change array
-      // this.playerOptions.sources = [{
-      //   type: 'application/x-mpegURL',
-      //   src: 'https://videodelivery.net/d030fb826be3b9395b9c46bfcd3eabf8/manifest/video.m3u8'
-      // }]
-      // this.player.muted(false)
-    }, 5000)
+    // this.$refs.videoPlayer.player.controlBar.progressControl.disable()
   },
   computed: {
     player () {
